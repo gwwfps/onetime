93bd15418b7335cba30b1c2388473d4af4039c54
David Zhang <gwwfps@gmail.com>
2012-09-23 15:15:20 -0400

    digit length validation

--- onetime.go
+++ onetime.go
@@ -4,6 +4,7 @@ import (
     "crypto/hmac"
     "crypto/sha1"
     "encoding/binary"
+    "errors"
     "math"
 )
 
@@ -27,6 +28,18 @@ func (otp *OneTimePassword) truncate(hs []byte) uint {
     snum := uint(sbits[3]) | uint(sbits[2])<<8
     snum |= uint(sbits[1])<<16 | uint(sbits[0])<<24
     return snum % uint(math.Pow(10, float64(otp.Digit)))
+}
+
+func New(digit int) (otp OneTimePassword, err error) {
+    if digit < 6 {
+        err = errors.New("A minimum of 6 digits is required for a valid HTOP code.")
+        return
+    } else if digit > 9 {
+        err = errors.New("An HTOP code cannot be longer than 9 digits.")
+        return
+    }
+    otp = OneTimePassword{digit}
+    return
 }
 
 func dt(hs []byte) []byte {
